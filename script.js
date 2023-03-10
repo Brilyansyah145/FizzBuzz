d52d07b3ae07e82bf44607e1675fd4eb730e3b0b
Ade Octa <ade.octa.brilyansyah2@gmail.com>
2023-03-11 06:51:29 +0700

    Update

--- script.js
+++ script.js
@@ -12,5 +12,7 @@ const getResult = function() {
             display.innerHTML+= "<div>" + "Fizz" + "</div>";
         } else if (number % 5 == 0) {
             display.innerHTML+= "<div>" + "Buzz" + "</div>";
-    }
+        } else {
+            display.innerHTML+= "<div>" + number + "</div>"
+        }
 }
